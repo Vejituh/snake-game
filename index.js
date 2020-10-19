5416881842c5b95cf6a24e18fb65bc503e34f3a2
Rafael Gomes <rafabear@bearmachine-share.broadband>
2020-10-19 15:29:22 +0100

    add move function and key even listener

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ let score  = 0;
 let applesEat = 0;
 let squares = [];
 let snake = [];
+let direction = 1;
+let timerId = 0;
+let tail = 0;
 
 function displayGrid() {
     for (let numOfSquares = 0; numOfSquares < 100; numOfSquares++) {
         let square = document.createElement('div');
         square.classList.add('square')
-        // square.textContent = numOfSquares
         grid.appendChild(square);
         squares.push(square)
     }
     displaySnake();
+    timerId = setInterval(move,1000);
 }
 
 function displaySnake() {
@@ -23,9 +26,42 @@ function displaySnake() {
         randomSnakeVal = randomSnakeVal - 3;
     }
     snake.push(randomSnakeVal, randomSnakeVal+1, randomSnakeVal+2);
+    snake.reverse();
     snake.forEach(element => {
         squares[element].classList.add('snake');
     });
 }
 
+function move() {
+        if(
+            snake[0] % 10 === 9 && direction === 1 ||
+            snake[0] % 10 === 0 && direction === -1 ||
+            snake[0] + 10 > 99 && direction === 10 ||
+            snake[0] - 10 < 0 && direction === -10 ||
+            squares[snake[0] + direction].classList.contains('snake')
+        )
+            {
+            clearInterval(timerId);
+        } else {
+                console.log(snake)
+                tail = snake.pop();
+                squares[tail].classList.remove('snake');
+                snake.unshift((snake[0] + direction));
+                squares[snake[0]].classList.add('snake');
+        } 
+}
+
 displayGrid();
+
+document.addEventListener('keyup',function(e) {
+    console.log(e.code);
+    if (e.code === 'ArrowUp') {
+        direction = -10;
+    } else if(e.code === 'ArrowDown') {
+        direction = 10;
+    } else if(e.code === 'ArrowLeft') {
+        direction = -1;
+    } else if(e.code === 'ArrowRight') {
+        direction = 1;
+    }
+})
